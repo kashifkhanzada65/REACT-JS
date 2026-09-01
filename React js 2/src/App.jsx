bd9ca716da155f427102782a5885e2e93f3205fc
kashifkhanzada65 <muhammadkashifkhanzada110@gmail.com>
2026-09-01 19:17:34 +0500

    react start full basic sa

---
+++ React js 2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navber from "./components/Navber";
+import Body from "./components/body";
+import Footer from "./components/footer";
+
+const App = () => {
+return(
+    <div>
+        <Navber />
+        <Body />
+        <Footer />
+    </div>
+)
+}
+export default App
---
+++ React js 2/src/components/navber.jsx
@@ -0,0 +1,9 @@
+const Navber = () => {
+return(
+    <div>
+        <h1>Navber</h1>
+    </div>
+)
+}
+
+export default Navber
