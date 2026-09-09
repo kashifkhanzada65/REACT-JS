a7db0213017d7742a7400fa5d9357d92648be734
kashifkhanzada65 <muhammadkashifkhanzada110@gmail.com>
2026-09-09 10:40:19 +0500

    usestate start

---
+++ React js 4/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+    // const userName = "Kashif"
+    // const isLogin = false
+
+
+    // USESTATE / STATE 
+
+    // let userName = "Kashif"
+    // console.log(userName);
+
+    // const loginHandler = () => {
+    //     userName = "Mehmood"
+    //     console.log(userName);
+    // }
+
+    // let [userName, setUserName] = useState("Kashif")
+    // // console.log(userName);
+    // const loginHandler = () => {
+    //     setUserName("Kashif Khanzada")
+    // }
+    // console.log(userName);
+
+
+    return (
+        <div>
+
+            {/* <h1 style={{
+                color: "red"
+            }}>{userName}</h1>
+             */}
+            {/* <h1>{isLogin ? userName : "Please login"}</h1> */}
+
+
+            {/* <h1>Hello {userName}</h1>
+            <button onClick={loginHandler}>login</button> */}
+        </div>
+    )
+}
+
+export default App
